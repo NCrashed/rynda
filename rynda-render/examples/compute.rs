64623e0c9e34f250efa0a5769dc75fa374281db8
Anton Gushcha <ncrashed@protonmail.com>
2022-12-23 20:53:08 +0700

    Make texture render as default

--- rynda-render/examples/compute.rs
+++ rynda-render/examples/compute.rs
@@ -1,28 +1,24 @@
 extern crate gl;
 extern crate glfw;
 
-use glam::UVec3;
 use glfw::{Action, Context, Key};
 use ndarray::Array3;
 use std::str;
 
 use rynda_format::types::{volume::RleVolume, voxel::RgbVoxel};
 use rynda_render::render::{
-    buffer::texture::Texture,
     debug::enable_gl_debug,
-    pipeline::{generic::Pipeline, quad::QuadPipeline, texture::TexturePipeline},
+    pipeline::{generic::Pipeline, quad::QuadPipeline, raycast::RaycastPipeline},
 };
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    glfw.window_hint(glfw::WindowHint::Resizable(false));
-    let width = 1024;
-    let height = 1024;
+    glfw.window_hint(glfw::WindowHint::Resizable(true));
     let (mut window, events) = glfw
         .create_window(
-            width,
-            height,
-            "Rynda texture target test",
+            1024,
+            1024,
+            "Rynda pointmap test",
             glfw::WindowMode::Windowed,
         )
         .expect("Failed to create GLFW window.");
@@ -37,8 +33,7 @@ fn main() {
 
     // let z = RgbVoxel::empty();
     // let r = RgbVoxel::only_red(1);
-    // let voxels: Array3<RgbVoxel> = ndarray::arr3(&[[[z, r], [z, r]], [[z, z], [z, z]]]);
-
+    // let voxels: Array3<RgbVoxel> = arr3(&[[[z, r], [z, r]], [[z, z], [z, z]]]);
     let voxels: Array3<RgbVoxel> = Array3::from_shape_fn((256, 256, 256), |(x, y, z)| {
         let sx = (x as isize) - 128;
         let sz = (z as isize) - 128;
@@ -49,23 +44,15 @@ fn main() {
             RgbVoxel::empty()
         }
     });
-
     let volume: RleVolume = voxels.into();
-    let pointmap_texture = Texture::from_pointermap(gl::TEXTURE0, &volume);
 
-    let quad_vertex = str::from_utf8(include_bytes!("../shaders/quad_vertex.glsl")).unwrap();
-    let quad_fragment = str::from_utf8(include_bytes!("../shaders/quad_fragment.glsl")).unwrap();
-    let texture_fragment =
-        str::from_utf8(include_bytes!("../shaders/pointermap_fragment.glsl")).unwrap();
-
-    let texture_pipeline = TexturePipeline::new(quad_vertex, texture_fragment, width, height);
-    texture_pipeline.program.print_uniforms();
-
-    let quad_pipeline = QuadPipeline::new(
-        quad_vertex,
-        quad_fragment,
-        &texture_pipeline.framebuffer.color_buffer,
-    );
+    let vertex_shader = str::from_utf8(include_bytes!("../shaders/quad_vertex.glsl")).unwrap();
+    let fragment_shader = str::from_utf8(include_bytes!("../shaders/quad_fragment.glsl")).unwrap();
+    let compute_shader =
+        str::from_utf8(include_bytes!("../shaders/pointermap_compute.glsl")).unwrap();
+    let raycast_pipeline = RaycastPipeline::new(compute_shader, &volume);
+    let quad_pipeline =
+        QuadPipeline::new(vertex_shader, fragment_shader, &raycast_pipeline.texture);
 
     let mut mode: u32 = 0;
     while !window.should_close() {
@@ -74,22 +61,15 @@ fn main() {
             handle_window_event(&mut window, event, &mut mode);
         }
 
-        texture_pipeline.bind();
         unsafe {
             // Clear the screen
             gl::ClearColor(1.0, 1.0, 1.0, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
-        texture_pipeline.program.set_uniform("mode", &(mode as i32));
-        texture_pipeline.program.set_uniform(
-            "volume_size",
-            &UVec3::new(volume.xsize, volume.ysize, volume.zsize),
-        );
-        pointmap_texture.bind(0);
 
-        texture_pipeline.program.set_uniform("pointermap", &0u32);
-        texture_pipeline.draw();
-
+        raycast_pipeline.bind();
+        raycast_pipeline.program.set_uniform("mode", &(mode as i32));
+        raycast_pipeline.draw();
         quad_pipeline.bind_draw();
 
         window.swap_buffers();
